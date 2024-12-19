53098ba07041cdd35bc8ed2565ddd62be3b17def
Shamil Lachenilaev <lachen1305@gmail.com>
2024-12-19 15:48:41 +0300

    Made changes to the file composition_implementation.py (Ввел изменения в файле composition_implementation.py)

--- homework/composition_implementation.py
+++ homework/composition_implementation.py
@@ -1,15 +1,13 @@
 from computer_components import PowerSupply, Motherboard, CPU, RAM, SSD, GraphicsCard
 
 class ComputerCase:
-    def __init__(
-        self, power, chipset, clock_speed, cores, memory_size_ram, frequency, capacity, model, memory_size_graf
-    ):
-        self.power_supply = PowerSupply(power)
-        self.motherboard = Motherboard(chipset)
-        self.cpu = CPU(clock_speed, cores)
-        self.ram = RAM(memory_size_ram, frequency)
-        self.ssd = SSD(capacity)
-        self.graphics_card = GraphicsCard(model, memory_size_graf)
+    def __init__(self, power_supply_obj, motherboard_obj, cpu_obj, ram_obj, ssd_obj, graphics_card_obj):
+        self.power_supply = power_supply_obj
+        self.motherboard = motherboard_obj
+        self.cpu = cpu_obj
+        self.ram = ram_obj
+        self.ssd = ssd_obj
+        self.graphics_card = graphics_card_obj
 
     def start_computer(self):
         return f'{self.power_supply.supply_voltage()}\n{self.motherboard.redistribute_voltage()}\n{self.graphics_card.display_image()}'
@@ -17,17 +15,14 @@ class ComputerCase:
 
 if __name__ == '__main__':
     print(f'\nФайл - {__file__.split('\\')[-1]}\n')
-    computer = ComputerCase(
-        750,
-        'AMD B650',
-        8,
-        3.5,
-        32,
-        5000,
-        1000,
-        'GeForce RTX 4090',
-        24
-    )
+    power_supply = PowerSupply(750)
+    motherboard = Motherboard('AMD B650')
+    cpu = CPU(8, 3.5)
+    ram = RAM(32, 5000)
+    ssd = SSD(1000)
+    graphics_card = GraphicsCard('GeForce RTX 4090', 24)
+
+    computer = ComputerCase(power_supply, motherboard, cpu, ram, ssd, graphics_card)
     print(computer.start_computer())
     print()
 
